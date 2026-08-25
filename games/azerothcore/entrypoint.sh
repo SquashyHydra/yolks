5fea2cb285a77cf35ae7805396dee6cad306f5eb
Matthew Hudek <3-SquashyHydra@gitlab.gnogle.co.za>
2026-08-25 21:35:19 +0200

    Add configuration file copying and ownership adjustment in entrypoint script

--- games/azerothcore/entrypoint.sh
+++ games/azerothcore/entrypoint.sh
@@ -8,6 +8,14 @@ cd /home/container
 # are owned by a different UID/GID.
 if [ "$(id -u)" = "0" ]; then
     chown -R container:container /home/container
+    if [ -f "/home/container/env/dist/etc/azerothcore.conf.dist" ]; then
+        cp /home/container/env/dist/etc/azerothcore.conf.dist /home/container/env/dist/etc/azerothcore.conf
+        chown container:container /home/container/env/dist/etc/azerothcore.conf
+    fi
+    if [ -f "/home/container/env/dist/etc/worldserver.conf.dist" ]; then
+        cp /home/container/env/dist/etc/worldserver.conf.dist /home/container/env/dist/etc/worldserver.conf
+        chown container:container /home/container/env/dist/etc/worldserver.conf
+    fi
     exec su -s /bin/bash container -c "$*"
 fi
 
