32db89cd0ab874d0140dc0d8f72b2aac6d5c4d41
Matthew Hudek <3-SquashyHydra@gitlab.gnogle.co.za>
2026-08-25 15:06:42 +0200

    Add Dockerfile and entrypoint script for Ubuntu 24.04 base image

---
+++ oses/ubuntu/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+cd /home/container
+
+# Fix permissions in case Pterodactyl mounted files
+# are owned by a different UID/GID.
+if [ "$(id -u)" = "0" ]; then
+    chown -R container:container /home/container
+    exec su -s /bin/bash container -c "$*"
+fi
+
+# If no command was supplied, open bash.
+if [ $# -eq 0 ]; then
+    exec /bin/bash
+fi
+
+# Execute the command supplied by Pterodactyl.
+exec "$@"
