99bf0d8fd10713f67b030b80568d429952b0459d
Matthew Hudek <3-SquashyHydra@gitlab.gnogle.co.za>
2026-08-25 23:17:24 +0200

    Refactor entrypoint script to ensure configuration files are copied correctly

--- games/azerothcore/entrypoint.sh
+++ games/azerothcore/entrypoint.sh
@@ -4,6 +4,17 @@ set -e
 
 cd /home/container
 
+if [ -f "/home/container/env/dist/etc/authserver.conf.dist" ]; then
+    cp /home/container/env/dist/etc/authserver.conf.dist /home/container/env/etc/authserver.conf
+else
+    echo "No authserver.conf.dist file found, this was not expected."
+fi
+if [ -f "/home/container/env/dist/etc/worldserver.conf.dist" ]; then
+    cp /home/container/env/dist/etc/worldserver.conf.dist /home/container/env/etc/worldserver.conf
+else
+    echo "No worldserver.conf.dist file found, this was not expected."
+fi
+
 # Fix permissions in case Pterodactyl mounted files
 # are owned by a different UID/GID.
 #if [ "$(id -u)" = "0" ]; then
