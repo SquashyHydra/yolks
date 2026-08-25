8b06a1361e5ecd88450a429d534fa0bc02cb4878
Matthew Hudek <3-SquashyHydra@gitlab.gnogle.co.za>
2026-08-25 22:33:30 +0200

    Comment out UID/GID check for container ownership adjustment in entrypoint script

--- games/azerothcore/entrypoint.sh
+++ games/azerothcore/entrypoint.sh
@@ -16,9 +16,9 @@ fi
 
 # Fix permissions in case Pterodactyl mounted files
 # are owned by a different UID/GID.
-if [ "$(id -u)" = "0" ]; then
-    chown -R container:container /home/container
-fi
+#if [ "$(id -u)" = "0" ]; then
+#    chown -R container:container /home/container
+#fi
 
 # If no command was supplied, open bash.
 if [ -z "$STARTUP" ]; then
